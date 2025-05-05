5c5d448680cb24ca3f5b87ca81731ab68a05075b
dev-gaurav <gaurav.singh@flairminds.com>
2025-05-05 16:23:56 +0530

    socket fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,9 +3,13 @@ import { BASE_URL } from "./urlConstant";
 
 export const createSocketConnection = () => {
     // connecting client to server
-    if(location.hostname === "localhost") {
-        return io(BASE_URL);
-    } else {
-        return io("/", { path: "/socket.io"})
-    }
+    const isLocal = location.hostname === "localhost";
+    
+    return io(isLocal ? "http://localhost:3000" : window.location.origin, {
+        path: "/socket.io", // required if server is using a custom path
+        transports: ["websocket"], // force WebSocket instead of long-polling
+        secure: !isLocal, // ensure secure connection on production
+        withCredentials: true, // optional: needed if your API uses cookies/auth
+    });
+
 }
